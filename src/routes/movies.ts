d8c00dbeed2667ed415858079d3056bde8523957
Marcos Melo <marcos@marcosmelo.tech>
2022-01-28 12:30:42 -0500

    find a movie by id: GET /movies/:id

--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import { collections } from '../services/database'
+import {ObjectId} from 'mongodb'
 
 export const movies = express.Router()
 
@@ -12,5 +13,18 @@ movies.get('/', async (_req: Request, res: Response) => {
         res.status(200).send(movies)
     } catch (error) {
         res.status(500).send(error.message)
+    }
+})
+
+movies.get('/:id', async (req: Request, res: Response) => {
+    const id = req?.params?.id
+    try {
+        const query = { _id: new ObjectId(id) }
+        const movie = await collections.movies.findOne(query)
+
+        if (movie) res.status(200).send(movie)
+
+    } catch (error) {
+        res.status(404).send(`Unable to find matching document with id: ${req.params.id}`)
     }
 })
